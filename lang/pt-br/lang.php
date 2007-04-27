2be5bdf24b8f01fdc666cd615d74ca143fbeeea4
wikidesign <chi@chimeric.de>
2007-04-27 13:15:35 +0200

    29 Brasilian Portuguese translation by Felipe Castro
    darcs-hash:20070427111535-89468-47dd22f596c04206b6160d2f8e802d0c0c09c7f3.gz

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Arquivo para o português
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Felipe Castro <fefcas@yahoo.com.br>
+ */
+
+// cadeias customizadas da língua para o pluguim
+$lang['page'] = 'Paĝo';
+$lang['date'] = 'Dato';
+$lang['user'] = 'Uzulo';
+$lang['desc'] = 'Priskribo';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
