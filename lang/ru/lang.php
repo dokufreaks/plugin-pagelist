febf2a10b03f125a10808e94161c29ddb529e1c5
Malamut <malamut@ubuntu.ru>
2010-03-22 11:23:14 +0100

    added russian translation

---
+++ lang/ru/lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Russian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Malamut (malamut@ubuntu.ru)
+ */
+
+// custom language strings for the plugin
+$lang['page'] = 'Страница';
+$lang['date'] = 'Дата изменения';
+$lang['user'] = 'Редактор';
+$lang['desc'] = 'Описание';
+//vim:ts=4:sw=4:et:enc=utf-8
---
+++ lang/ru/settings.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Russian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Malamut (malamut@ubuntu.ru)
+ */
+ 
+// for the configuration manager
+$lang['style']            = 'Стиль генерируемого списка страниц';
+$lang['style_o_default']  = 'по умолчанию';
+$lang['style_o_table']    = 'таблица';
+$lang['style_o_list']     = 'список';
+
+$lang['showheader']       = 'Показывать заголовок';
+$lang['showdate']         = 'Дата';
+$lang['showdate_o_0']     = 'скрыть';
+$lang['showdate_o_1']     = 'дата создания';
+$lang['showdate_o_2']     = 'дата модификации';
+$lang['showuser']         = 'Информация о пользователе';
+$lang['showuser_o_0']     = 'скрыть';
+$lang['showuser_o_1']     = 'создатель';
+$lang['showuser_o_2']     = 'редакторы';
+$lang['showdesc']         = 'Описание';
+$lang['showdesc_o_0']     = 'скрыть';
+$lang['showdesc_o_160']   = 'макс. 160 символов';
+$lang['showdesc_o_500']   = 'макс. 500 символов';
+$lang['showcomments']     = 'Показывать комментарии (требуется Discussion Plugin)';
+$lang['showlinkbacks']    = 'Показывать обратные ссылки (требуется Linkback Plugin)';
+$lang['showtags']         = 'Показывать теги (требуется Tag Plugin)';
+$lang['showfirsthl']      = 'Показывать первый заголовок вместо названия статьи';
+//vim:ts=4:sw=4:et:enc=utf-8
