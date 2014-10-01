e6a810ed14580f95cd2930f96f94208ebb194ea3
SC Yoo <dryoo@live.com>
2014-10-01 12:30:40 +0900

    korean

---
+++ lang/ko/lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Japanese language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+
+ */
+
+// custom language strings for the plugin
+$lang['page'] = '페이지'; 
+$lang['date'] = '날짜'; 
+$lang['user'] = '사용자'; 
+$lang['desc'] = '내용';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
