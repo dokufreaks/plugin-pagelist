7f9304e4f8d0241a2166c7bca2ed38e6f2bbec3a
SC Yoo <dryoo@live.com>
2014-10-26 00:42:34 +0900

    korean settings.php

---
+++ lang/ko/settings.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Korean language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     S.C.Yoo <dryoo@live.com> 
+ */
+ 
+// for the configuration manager
+$lang['style'] = 'Pagelist 테이블 형식'; 
+$lang['style_o_default'] = '기본'; 
+$lang['style_o_table'] = '테이블'; 
+$lang['style_o_list'] = '목록'; 
+$lang['showheader'] = '헤더 행을 표시'; 
+$lang['showdate'] = '날짜 표시'; 
+$lang['showdate_o_0'] = '숨기기'; 
+$lang['showdate_o_1'] = '작성일'; 
+$lang['showdate_o_2'] = '수정 한 날짜'; 
+$lang['showuser'] = '사용자를 표시'; 
+$lang['showuser_o_0'] = '숨기기'; 
+$lang['showuser_o_1'] = '작성자'; 
+$lang['showuser_o_2'] = '편집자'; 
+$lang['showdesc'] = '내용을 표시'; 
+$lang['showdesc_o_0'] = '숨기기'; 
+$lang['showdesc_o_160'] = '최대 160 문자'; 
+$lang['showdesc_o_500'] = '최대 500 문자'; 
+$lang['showcomments'] = '코멘트보기 (Discussion 플러그인이 필요)'; 
+$lang['showlinkbacks'] = '링크 벡보기 (Linkback 플러그인이 필요)'; 
+$lang['showtags'] = '태그를 표시 (Tag 플러그인이 필요)';
+$lang['showfirsthl']      = '페이지 이름 대신 첫 제목줄을 표시';
+$lang['sort']             = '페이지이름 순서로 정렬';
+$lang['rsort']            = '페이지 이름의 역순으로 정렬';
+
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
