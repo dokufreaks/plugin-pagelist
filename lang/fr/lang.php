5f260d3bd7021670a583109e3e966ba3337c4d74
trebmuh <trebmuh@users.noreply.github.com>
2015-07-24 15:58:24 +0200

    Update lang.php
    FR translation

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,1 +1,13 @@
-
+<?php
+/**
+* French language file
+*
+* @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+* @author Olivier Humbert <trebmuh@tuxfamily.org>
+*/
+// custom language strings for the plugin
+$lang['page'] = 'Page';
+$lang['date'] = 'Date';
+$lang['user'] = 'Utilisateur';
+$lang['desc'] = 'Description';
+//Setup VIM: ex: et ts=2 enc=utf-8 :
