bfd902720d3ab6f85acc35359dd17818025c5162
trebmuh <trebmuh@users.noreply.github.com>
2015-07-24 16:04:01 +0200

    Update settings.php
    FR translation

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,1 +1,32 @@
-
+<?php
+/**
+* French language file
+*
+* @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+* @author Olivier Humbert <trebmuh@tuxfamily.org>
+*/
+// for the configuration manager
+$lang['style'] = 'Style des tableaux de pagelist';
+$lang['style_o_default'] = 'défault';
+$lang['style_o_table'] = 'tableau';
+$lang['style_o_list'] = 'liste';
+$lang['showheader'] = 'affiche la ligne d'entête';
+$lang['showdate'] = 'affiche la colonne de date';
+$lang['showdate_o_0'] = 'cache';
+$lang['showdate_o_1'] = 'date de création';
+$lang['showdate_o_2'] = 'date de modification';
+$lang['showuser'] = 'affiche la colonne d'utilisateur';
+$lang['showuser_o_0'] = 'cache';
+$lang['showuser_o_1'] = 'créateur';
+$lang['showuser_o_2'] = 'contributeurs';
+$lang['showdesc'] = 'affiche la colonne de description';
+$lang['showdesc_o_0'] = 'cache';
+$lang['showdesc_o_160'] = '160 caractères maximum';
+$lang['showdesc_o_500'] = '500 caractères maximum';
+$lang['showcomments'] = 'affiche la colonne de commentaire ("Discussion Plugin" nécessaire)';
+$lang['showlinkbacks'] = 'affiche la colonne de liens de retour ("Linkback Plugin" nécessaire)';
+$lang['showtags'] = 'affiche la colonne de tags ("Tag Plugin" nécessaire)';
+$lang['showfirsthl'] = 'affiche la première ligne d'entête plutôt que le nom de la page';
+$lang['sort'] = 'trie les pages par "pagename"';
+$lang['rsort'] = 'inverse le tri des pages par "pagename"';
+//Setup VIM: ex: et ts=2 :
