418d10424cd4cae14b755673ea5bda4816bc6b05
Domingo Redal <docxml@gmail.com>
2015-08-01 12:45:32 +0200

    translation update

--- lang/es/settings.php
+++ lang/es/settings.php
@@ -1,31 +1,31 @@
-﻿<?php
+<?php
+
 /**
- * Spanish language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Herman Fabián Sandoval Manrique <hfsandovalm@emzac.com>
+ * 
+ * @author Herman Fabián Sandoval Manrique <hfsandovalm@emzac.com>
+ * @author Domingo Redal <docxml@gmail.com>
  */
- 
-// for the configuration manager
-$lang['style']            = 'estilo de las tablas pagelist';
-$lang['style_o_default']  = 'por defecto';
-$lang['style_o_table']    = 'tabla';
-$lang['style_o_list']     = 'lista';
-
-$lang['showheader']       = 'mostrar línea de encabezado';
-$lang['showdate']         = 'mostrar columna de fecha';
-$lang['showdate_o_0']     = 'ocultar';
-$lang['showdate_o_1']     = 'fecha de creación';
-$lang['showdate_o_2']     = 'fecha de modificación';
-$lang['showuser']         = 'mostrar columna de usuario';
-$lang['showuser_o_0']     = 'ocultar';
-$lang['showuser_o_1']     = 'creador';
-$lang['showuser_o_2']     = 'contributors';
-$lang['showdesc']         = 'mostrar columna de descripción';
-$lang['showdesc_o_0']     = 'ocultar';
-$lang['showdesc_o_160']   = 'max. 160 caracteres';
-$lang['showdesc_o_500']   = 'max. 500 caracteres';
-$lang['showcomments']     = 'mostrar columna de comentarios (Se requiere el plugin Discussion)';
-$lang['showtags']         = 'mostrar columna de etiquetas (Se requiere el plugin Tag)';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['style']                 = 'estilo de las tablas pagelist';
+$lang['style_o_default']       = 'por defecto';
+$lang['style_o_table']         = 'tabla';
+$lang['style_o_list']          = 'lista';
+$lang['showheader']            = 'mostrar línea de encabezado';
+$lang['showdate']              = 'mostrar columna de fecha';
+$lang['showdate_o_0']          = 'ocultar';
+$lang['showdate_o_1']          = 'fecha de creación';
+$lang['showdate_o_2']          = 'fecha de modificación';
+$lang['showuser']              = 'mostrar columna de usuario';
+$lang['showuser_o_0']          = 'ocultar';
+$lang['showuser_o_1']          = 'creador';
+$lang['showuser_o_2']          = 'contributors';
+$lang['showdesc']              = 'mostrar columna de descripción';
+$lang['showdesc_o_0']          = 'ocultar';
+$lang['showdesc_o_160']        = 'max. 160 caracteres';
+$lang['showdesc_o_500']        = 'max. 500 caracteres';
+$lang['showcomments']          = 'mostrar columna de comentarios (Se requiere el plugin Discussion)';
+$lang['showlinkbacks']         = 'mostrar columna LinkBacks (Se requiere el plugin Linkback)';
+$lang['showtags']              = 'mostrar columna de etiquetas (Se requiere el plugin Tag)';
+$lang['showfirsthl']           = 'mostrar el primer título en lugar del nombre de la página';
+$lang['sort']                  = 'ordenar páginas por nombre de la página';
+$lang['rsort']                 = 'orden descendente de páginas por nombre de la página';
