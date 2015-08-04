edf6fb89ba6210abee67dd2975a6cdff8ef3e317
Myeongjin <aranet100@gmail.com>
2015-08-04 12:35:45 +0200

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -1,16 +1,12 @@
 <?php
+
 /**
- * Korean language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     S.C.Yoo <dryoo@live.com> 
+ * 
+ * @author S.C.Yoo <dryoo@live.com>
+ * @author Myeongjin <aranet100@gmail.com>
  */
-
-
-// custom language strings for the plugin
-$lang['page'] = '페이지'; 
-$lang['date'] = '날짜'; 
-$lang['user'] = '사용자'; 
-$lang['desc'] = '내용';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['page']                  = '문서';
+$lang['date']                  = '날짜';
+$lang['user']                  = '사용자';
+$lang['desc']                  = '설명';
