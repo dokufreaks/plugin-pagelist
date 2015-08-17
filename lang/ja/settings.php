fd7bacb3788776869b0828ab47987bbd9816ab00
Hideaki SAWADA <chuno@live.jp>
2015-08-17 08:15:37 +0200

    translation update

--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -1,36 +1,32 @@
 <?php
+
 /**
- * Japanese language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Ikuo Obataya <cxx05051@nifty.com>
- *             Hideaki SAWADA <chuno@live.jp>
+ * 
+ * @author Ikuo Obataya <cxx05051@nifty.com>
+ * @author Hideaki SAWADA <chuno@live.jp>
  */
- 
-// for the configuration manager
-$lang['style']            = 'Pagelistテーブル形式';
-$lang['style_o_default']  = 'デフォルト';
-$lang['style_o_table']    = 'テーブル';
-$lang['style_o_list']     = 'リスト';
-
-$lang['showheader']       = 'ヘッダ行を表示';
-$lang['showdate']         = '日付を表示';
-$lang['showdate_o_0']     = '隠す';
-$lang['showdate_o_1']     = '作成日';
-$lang['showdate_o_2']     = '修正日';
-$lang['showuser']         = 'ユーザーを表示';
-$lang['showuser_o_0']     = '隠す';
-$lang['showuser_o_1']     = '作成者';
-$lang['showuser_o_2']     = '編集者';
-$lang['showdesc']         = '内容を表示';
-$lang['showdesc_o_0']     = '隠す';
-$lang['showdesc_o_160']   = '最大160文字';
-$lang['showdesc_o_500']   = '最大500文字';
-$lang['showcomments']     = 'コメントを表示(Discussionプラグインが必要)';
-$lang['showlinkbacks']    = 'リンクベックを表示(Linkbackプラグインが必要)';
-$lang['showtags']         = 'タグを表示(Tagプラグインが必要)';
-$lang['showfirsthl']      = '最初の見出しをページ名とする';
-$lang['sort']             = 'ページ名でソート';
-$lang['rsort']            = 'ページ名で逆ソート';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['style']                 = 'Pagelist の表示形式';
+$lang['style_o_default']       = '横罫線のみのテーブル（デフォルト）';
+$lang['style_o_table']         = 'テーブル';
+$lang['style_o_list']          = 'リスト';
+$lang['showheader']            = 'ヘッダ行を表示する';
+$lang['showdate']              = '日付を表示する';
+$lang['showdate_o_0']          = '非表示';
+$lang['showdate_o_1']          = '作成日';
+$lang['showdate_o_2']          = '変更日';
+$lang['showuser']              = 'ユーザーを表示する
+';
+$lang['showuser_o_0']          = '非表示';
+$lang['showuser_o_1']          = '作成者';
+$lang['showuser_o_2']          = '編集者';
+$lang['showdesc']              = '内容を表示する';
+$lang['showdesc_o_0']          = '非表示';
+$lang['showdesc_o_160']        = '最大 160 文字';
+$lang['showdesc_o_500']        = '最大 500 文字';
+$lang['showcomments']          = 'コメントを表示する(Discussionプラグインが必要)';
+$lang['showlinkbacks']         = 'リンクベックを表示する(Linkbackプラグインが必要)';
+$lang['showtags']              = 'タグを表示する(Tagプラグインが必要)';
+$lang['showfirsthl']           = '最初の見出しをページ名とする';
+$lang['sort']                  = 'ページ名でソート';
+$lang['rsort']                 = 'ページ名で逆ソート';
