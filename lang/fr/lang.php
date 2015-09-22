019bc36ecc815718da2c35fbc419f94cf8de7293
Nicolas Friedli <nicolas@theologique.ch>
2015-09-22 14:21:30 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,13 +1,11 @@
 <?php
+
 /**
-* French language file
-*
-* @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
-* @author Olivier Humbert <trebmuh@tuxfamily.org>
-*/
-// custom language strings for the plugin
-$lang['page'] = 'Page';
-$lang['date'] = 'Date';
-$lang['user'] = 'Utilisateur';
-$lang['desc'] = 'Description';
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Olivier Humbert <trebmuh@tuxfamily.org>
+ */
+$lang['page']                  = 'Page';
+$lang['date']                  = 'Date';
+$lang['user']                  = 'Utilisateur';
+$lang['desc']                  = 'Description';
