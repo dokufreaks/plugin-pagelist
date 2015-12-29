e6315011b3b449727d7221ef23811b5b77f5d2ac
Aleksandr Selivanov <alexgearbox@yandex.ru>
2015-12-29 17:15:28 +0100

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -4,8 +4,12 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Aleksandr Selivanov <alexgearbox@gmail.com>
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
 $lang['page']                  = 'Страница';
 $lang['date']                  = 'Дата';
 $lang['user']                  = 'Участник';
 $lang['desc']                  = 'Описание';
+$lang['diff']                  = 'Отличия';
+$lang['diff_title']            = 'Показать отличия от текущей версии';
+$lang['diff_alt']              = 'Показать отличия от текущей версии';
