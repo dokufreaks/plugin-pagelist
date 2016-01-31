41091f57559478fb9ca9ea8faeda2b602c5850ad
Dominik Eckelmann <deckelmann@gmail.com>
2016-01-31 15:50:06 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
- * German language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
+ * 
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author Dominik Eckelmann <deckelmann@gmail.com>
  */
-
-// custom language strings for the plugin
-$lang['page'] = 'Seite';
-$lang['date'] = 'Datum';
-$lang['user'] = 'Benutzer';
-$lang['desc'] = 'Beschreibung';
-$lang['diff'] = 'Unterschiede';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['page']                  = 'Seite';
+$lang['date']                  = 'Datum';
+$lang['user']                  = 'Benutzer';
+$lang['desc']                  = 'Beschreibung';
+$lang['diff']                  = 'Unterschiede';
+$lang['diff_title']            = 'Zeige unterschiede zur aktuellen Version';
+$lang['diff_alt']              = 'Zeige unterschiede zur aktuellen Version';
--- lang/de/settings.php
+++ lang/de/settings.php
@@ -1,37 +1,33 @@
 <?php
+
 /**
- * German language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
+ * 
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author Dominik Eckelmann <deckelmann@gmail.com>
  */
- 
-// for the configuration manager
-$lang['style']              = 'Stil der Seitenlisten';
-$lang['style_o_default']    = 'standard';
-$lang['style_o_table']      = 'Tabelle';
-$lang['style_o_list']       = 'Tabelle/Liste';
-$lang['style_o_simplelist'] = 'Einfache Liste';
-
-$lang['showheader']       = 'Tabellenkopf anzeigen';
-$lang['showdate']         = 'Datum anzeigen';
-$lang['showdate_o_0']     = 'ausblenden';
-$lang['showdate_o_1']     = 'Erstellungsdatum';
-$lang['showdate_o_2']     = 'Änderungsdatum';
-$lang['showuser']         = 'Benutzer anzeigen';
-$lang['showuser_o_0']     = 'ausblenden';
-$lang['showuser_o_1']     = 'Ersteller';
-$lang['showuser_o_2']     = 'Beitragende';
-$lang['showdesc']         = 'Beschreibung anzeigen';
-$lang['showdesc_o_0']     = 'ausblenden';
-$lang['showdesc_o_160']   = 'max. 160 Zeichen';
-$lang['showdesc_o_500']   = 'max. 500 Zeichen';
-$lang['showcomments']     = 'Kommentare anzeigen (Discussion Plugin wird benötigt)';
-$lang['showlinkbacks']    = 'Linkbacks anzeigen (Linkback Plugin wird benötigt)';
-$lang['showtags']         = 'Tags anzeigen (Tag Plugin wird benötigt)';
-$lang['showfirsthl']      = 'Erste Überschrift anstatt des Seitennamens anzeigen';
-$lang['showdiff']         = 'Link auf Diff-Seite anzeigen';
-$lang['sort']             = 'Seiten alphabetisch nach Namen sortieren';
-$lang['rsort']            = 'Seiten in umgekehrter alphabetischer Reihenfolge nach Namen sortieren';
-
-//Setup VIM: ex: et ts=2 :
+$lang['style']                 = 'Stil der Seitenlisten';
+$lang['style_o_default']       = 'standard';
+$lang['style_o_table']         = 'Tabelle';
+$lang['style_o_list']          = 'Tabelle/Liste';
+$lang['style_o_simple']        = 'Einfache Liste';
+$lang['showheader']            = 'Tabellenkopf anzeigen';
+$lang['showdate']              = 'Datum anzeigen';
+$lang['showdate_o_0']          = 'ausblenden';
+$lang['showdate_o_1']          = 'Erstellungsdatum';
+$lang['showdate_o_2']          = 'Änderungsdatum';
+$lang['showuser']              = 'Benutzer anzeigen';
+$lang['showuser_o_0']          = 'ausblenden';
+$lang['showuser_o_1']          = 'Ersteller';
+$lang['showuser_o_2']          = 'Beitragende';
+$lang['showdesc']              = 'Beschreibung anzeigen';
+$lang['showdesc_o_0']          = 'ausblenden';
+$lang['showdesc_o_160']        = 'max. 160 Zeichen';
+$lang['showdesc_o_500']        = 'max. 500 Zeichen';
+$lang['showcomments']          = 'Kommentare anzeigen (Discussion Plugin wird benötigt)';
+$lang['showlinkbacks']         = 'Linkbacks anzeigen (Linkback Plugin wird benötigt)';
+$lang['showtags']              = 'Tags anzeigen (Tag Plugin wird benötigt)';
+$lang['showfirsthl']           = 'Erste Überschrift anstatt des Seitennamens anzeigen';
+$lang['showdiff']              = 'Link auf Diff-Seite anzeigen';
+$lang['sort']                  = 'Seiten alphabetisch nach Namen sortieren';
+$lang['rsort']                 = 'Seiten in umgekehrter alphabetischer Reihenfolge nach Namen sortieren';
