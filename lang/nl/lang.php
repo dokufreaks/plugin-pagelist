ee0e6243aa22d660c6ccbddd7ad0631c4a2a0f95
Hugo Smet <hugo.smet@scarlet.be>
2016-02-08 05:35:05 +0100

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -1,16 +1,16 @@
 <?php
+
 /**
- * Dutch language file.
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
- * @author     Mark C. Prins <mprins@users.sf.net>
+ * 
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author Mark C. Prins <mprins@users.sf.net>
+ * @author Hugo Smet <hugo.smet@scarlet.be>
  */
-
-// custom language strings for the plugin
-$lang['page'] = 'Pagina';
-$lang['date'] = 'Datum';
-$lang['user'] = 'Gebruiker';
-$lang['desc'] = 'Beschrijving';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['page']                  = 'Pagina';
+$lang['date']                  = 'Datum';
+$lang['user']                  = 'Gebruiker';
+$lang['desc']                  = 'Beschrijving';
+$lang['diff']                  = 'Verschillen';
+$lang['diff_title']            = 'Toon verschillen met de huidige revisies';
+$lang['diff_alt']              = 'Toon verschillen met de huidige revisies';
