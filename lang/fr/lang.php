38476d98b2f0c2a74ae8fe72004f050e0491c89e
Laynee <seedfloyd@gmail.com>
2016-03-10 05:05:59 +0100

    french translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,11 +1,18 @@
 <?php
-
 /**
+ * French language file
+ *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
- * @author Olivier Humbert <trebmuh@tuxfamily.org>
+ * @author     Olivier Humbert <trebmuh@tuxfamily.org>
+ * @author     Laynee <seedfloyd@gmail.com>
  */
-$lang['page']                  = 'Page';
-$lang['date']                  = 'Date';
-$lang['user']                  = 'Utilisateur';
-$lang['desc']                  = 'Description';
+
+// custom language strings for the plugin
+$lang['page']       = 'Page';
+$lang['date']       = 'Date';
+$lang['user']       = 'Utilisateur';
+$lang['desc']       = 'Description';
+$lang['diff']       = 'Différences';
+$lang['diff_title'] = 'Différences avec la version précédente';
+$lang['diff_alt']   = 'Différences avec la version précédente';
+//Setup VIM: ex: et ts=2 enc=utf-8 :
