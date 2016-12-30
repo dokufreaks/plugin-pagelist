2005cfccd4e8b7462734ff9d874372fb42e06fab
Arne Hanssen <arne.hanssen@getmail.no>
2016-12-30 13:05:35 +0100

    translation update

---
+++ lang/no/settings.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Arne Hanssen <arne.hanssen@getmail.no>
+ */
+$lang['style']                 = 'Pagelist-stil';
+$lang['style_o_default']       = 'standard';
+$lang['style_o_table']         = 'tabell';
+$lang['style_o_list']          = 'tabell/liste';
+$lang['style_o_simple']        = 'enkel liste';
+$lang['showheader']            = 'vis tabellhoder';
+$lang['showdate']              = 'vis datokolonne';
+$lang['showdate_o_0']          = 'skjul';
+$lang['showdate_o_1']          = 'opprettelses dato';
+$lang['showdate_o_2']          = 'dato for endring';
+$lang['showuser']              = 'vis kolonne med bruker';
+$lang['showuser_o_0']          = 'skjul';
+$lang['showuser_o_1']          = 'opprettet av';
+$lang['showuser_o_2']          = 'bidragsytere';
+$lang['showdesc']              = 'vis beskrivelseskolonne';
+$lang['showdesc_o_0']          = 'skjul';
+$lang['showdesc_o_160']        = 'maks. 160 tegn';
+$lang['showdesc_o_500']        = 'maks. 500 tegn';
+$lang['showcomments']          = 'vis kolonne for kommentar, (Avhengig av discussion-utvidelse)';
+$lang['showlinkbacks']         = 'vis kolonne for tilbakelenker, (Avhengig av Linkback-utvidelse)';
+$lang['showtags']              = 'vis kolonne for emneknagg (Avhengig av tag-utvidelse)';
+$lang['showfirsthl']           = 'vis første overskrift istedetfor sidenavn';
+$lang['showdiff']              = 'vis lenke til side som viser forskjeller mellom versjoner av siden';
+$lang['sort']                  = 'sorter etter sidenavn';
+$lang['rsort']                 = 'sorter etter sidenavn i omvendt rekkefølge';
