3284861187009152a5b1ca13ec209e1370d86a60
LarsDW223 <lars_paulsen@web.de>
2017-08-26 15:02:29 +0200

    Fixed call to removed function 'reset()'. See #98.

--- syntax.php
+++ syntax.php
@@ -81,7 +81,6 @@ class syntax_plugin_pagelist extends DokuWiki_Syntax_Plugin {
         // for XHTML output
         if ($mode == 'xhtml') {
             if (!$my =& plugin_load('helper', 'pagelist')) return false;
-            $my->reset();
             $my->setFlags($flags);
             $my->startList();
             
