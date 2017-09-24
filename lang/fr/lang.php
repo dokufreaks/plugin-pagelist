6066becc38eea9c19e656cf58c25f59e6d0401f4
Schplurtz le Déboulonné <schplurtz@laposte.net>
2017-09-24 10:05:26 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,18 +1,15 @@
 <?php
-/**
- * French language file
- *
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Olivier Humbert <trebmuh@tuxfamily.org>
- * @author     Laynee <seedfloyd@gmail.com>
- */
 
-// custom language strings for the plugin
-$lang['page']       = 'Page';
-$lang['date']       = 'Date';
-$lang['user']       = 'Utilisateur';
-$lang['desc']       = 'Description';
-$lang['diff']       = 'Différences';
-$lang['diff_title'] = 'Différences avec la version précédente';
-$lang['diff_alt']   = 'Différences avec la version précédente';
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Olivier Humbert <trebmuh@tuxfamily.org>
+ * @author Laynee <seedfloyd@gmail.com>
+ */
+$lang['page']                  = 'Page';
+$lang['date']                  = 'Date';
+$lang['user']                  = 'Utilisateur';
+$lang['desc']                  = 'Description';
+$lang['diff']                  = 'Différences';
+$lang['diff_title']            = 'Différences avec la version précédente';
+$lang['diff_alt']              = 'Différences avec la version précédente';
