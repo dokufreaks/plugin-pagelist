786379affae90351d7527ac05c2da550cbc66ee1
qezwan <qezwan@gmail.com>
2020-10-06 12:05:09 +0200

    translation update

---
+++ lang/ckb/settings.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author qezwan <qezwan@gmail.com>
+ */
+$lang['style']                 = 'شێوازی لیستی لاپەڕە';
+$lang['style_o_default']       = 'بنەڕەت';
+$lang['style_o_table']         = 'خشتە';
+$lang['style_o_list']          = 'خشتە/لیست';
+$lang['style_o_simple']        = 'لیستی سادە';
+$lang['showheader']            = 'پیشاندانی هێڵی ناونیشان';
+$lang['showdate']              = 'ستوونی بەروار پیشان بدە';
+$lang['showdate_o_0']          = 'بشارەوە';
+$lang['showdate_o_1']          = 'بەرواری دروست کردن';
+$lang['showdate_o_2']          = 'بەرواری هەموارکردن';
+$lang['showuser']              = 'ستوونی بەکارهێنەر نیشان بدە';
+$lang['showuser_o_0']          = 'بشارەوە';
+$lang['showuser_o_1']          = 'دروستکەر';
+$lang['showuser_o_2']          = 'بەشداران';
+$lang['showdesc']              = 'پیشاندانی ستوونی وەسف';
+$lang['showdesc_o_0']          = 'بشارەوە';
+$lang['showdesc_o_160']        = 'زۆرینە. 160 نووسە';
+$lang['showdesc_o_500']        = 'زۆرینە. 500 نووسە';
+$lang['showcomments']          = 'ستوونی سەرنجەکان نیشان بدە (پێوەکراوی گفتوگۆ پێویستە)';
+$lang['showlinkbacks']         = 'ستوونی linkback نیشان بدە (پێوەکراوی Linkback پێویستە)';
+$lang['showtags']              = 'نیشاندانی ستوونی تاگەکان (تاگی پێوەکراو پێویستە)';
+$lang['showfirsthl']           = 'یەکەم سەردێڕ نیشان بدە لەجیاتی ناوی لاپەڕەکە';
+$lang['showimage']             = 'پیشاندانی ستوونی وێنە (پێوەکراوی Pageimage پێویستە)';
+$lang['showdiff']              = 'نیشاندانی لینک بۆ لاپەڕەی جیاکراوە';
+$lang['sort']                  = 'پۆلێنکردنی پەڕەکان بەپێی ناوی لاپەڕە';
+$lang['rsort']                 = 'پێچەوانەکردنی لاپەڕەکان بەپێی ناوی لاپەڕە';
