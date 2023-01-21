ee4a6eb73230b025d590e33fb11235770408051d
Minh <phandinhminh@protonmail.ch>
2023-01-22 00:30:28 +0100

    translation update

---
+++ lang/vi/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Minh <phandinhminh@protonmail.ch>
+ */
+$lang['page']                  = 'Trang';
+$lang['date']                  = 'Ngày tháng';
+$lang['user']                  = 'Người dùng';
+$lang['desc']                  = 'Mô tả';
+$lang['diff']                  = 'Khác biệt';
+$lang['diff_title']            = 'Hiện các khác biệt với các sửa đổi hiện thời';
+$lang['diff_alt']              = 'Hiện các khác biệt với các sửa đổi hiện thời';
