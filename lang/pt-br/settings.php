69da69e1906f27170041e69cc20cb7d536d5b97b
Gilson Caldeira <gilsoncaldeira@gmail.com>
2023-03-29 20:20:35 +0200

    translation update

--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -1,31 +1,38 @@
 <?php
+
 /**
  * Arquivo para o português
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Felipe Castro <fefcas@yahoo.com.br>
+ *
+ * @author Gilson Caldeira <gilsoncaldeira@gmail.com>
+ * @author Felipe Castro <fefcas@yahoo.com.br>
  */
- 
-// para o gerenciador de configurações
-$lang['style']            = 'Estilo da tabela para a lista de páginas';
-$lang['style_o_default']  = 'original';
-$lang['style_o_table']    = 'tabela';
-$lang['style_o_list']     = 'lista';
-
-$lang['showheader']       = 'mostrar cabeçalho';
-$lang['showdate']         = 'mostrar coluna de data';
-$lang['showdate_o_0']     = 'esconder';
-$lang['showdate_o_1']     = 'data de criação';
-$lang['showdate_o_2']     = 'data de modificação';
-$lang['showuser']         = 'mostrar coluna de usuário';
-$lang['showuser_o_0']     = 'esconder';
-$lang['showuser_o_1']     = 'criador';
-$lang['showuser_o_2']     = 'contribuidores';
-$lang['showdesc']         = 'mostrar coluna de descrição';
-$lang['showdesc_o_0']     = 'esconder';
-$lang['showdesc_o_160']   = 'max. 160 caracteres';
-$lang['showdesc_o_500']   = 'max. 500 caracteres';
-$lang['showcomments']     = 'mostrar coluna de comentários (precisa do "Discussion Plugin")';
-$lang['showtags']         = 'mostrar coluna de etiquetas (precisa do "Tag Plugin")';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['style']                 = 'Estilo da tabela para a lista de páginas';
+$lang['style_o_default']       = 'original';
+$lang['style_o_table']         = 'tabela';
+$lang['style_o_list']          = 'lista';
+$lang['style_o_simple']        = 'lista simples';
+$lang['showheader']            = 'mostrar cabeçalho';
+$lang['showdate']              = 'mostrar coluna de data';
+$lang['showdate_o_0']          = 'esconder';
+$lang['showdate_o_1']          = 'data de criação';
+$lang['showdate_o_2']          = 'data de modificação';
+$lang['showuser']              = 'mostrar coluna de usuário';
+$lang['showuser_o_0']          = 'esconder';
+$lang['showuser_o_1']          = 'criador';
+$lang['showuser_o_2']          = 'contribuidores';
+$lang['showuser_o_3']          = 'criador (no formato "showuseras")';
+$lang['showuser_o_4']          = 'contribuidores (no formato "showuseras")';
+$lang['showdesc']              = 'mostrar coluna de descrição';
+$lang['showdesc_o_0']          = 'esconder';
+$lang['showdesc_o_160']        = 'max. 160 caracteres';
+$lang['showdesc_o_500']        = 'max. 500 caracteres';
+$lang['showcomments']          = 'mostrar coluna de comentários (precisa do "Discussion Plugin")';
+$lang['showlinkbacks']         = 'mostre a coluna de links de retorno (o plug-in de linkback é necessário)';
+$lang['showtags']              = 'mostrar coluna de etiquetas (precisa do "Tag Plugin")';
+$lang['showfirsthl']           = 'mostrar o primeiro título em vez do nome da página';
+$lang['showimage']             = 'mostre a coluna da imagem (é necessário o plug-in Pageimage)';
+$lang['showdiff']              = 'mostre o link para a página de diferenças';
+$lang['sort']                  = 'classificar páginas por nome de página';
+$lang['rsort']                 = 'páginas de classificação reversa por nome de página';
