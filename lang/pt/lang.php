cb96d112f70ba907f6009da533fe96ed3b9d3415
Daniel Dias Rodrigues <danieldiasr@gmail.com>
2023-05-24 14:30:16 +0200

    translation update

--- lang/pt/lang.php
+++ lang/pt/lang.php
@@ -1,15 +1,17 @@
 <?php
+
 /**
  * Portuguese language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Fernando Ribeiro <pinguim.ribeiro@gmail.com>
+ *
+ * @author Daniel Dias Rodrigues <danieldiasr@gmail.com>
+ * @author Fernando Ribeiro <pinguim.ribeiro@gmail.com>
  */
-
-// custom language strings for the plugin
-$lang['page'] = 'Página';
-$lang['date'] = 'Data';
-$lang['user'] = 'Utilizador';
-$lang['desc'] = 'Descrição';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['page']                  = 'Página';
+$lang['date']                  = 'Data';
+$lang['user']                  = 'Utilizador';
+$lang['desc']                  = 'Descrição';
+$lang['diff']                  = 'Diferenças';
+$lang['diff_title']            = 'Mostrar diferenças para as revisões atuais';
+$lang['diff_alt']              = 'Mostrar diferenças para as revisões atuais';
