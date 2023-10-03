694672f5c5bb5200b69ef3e7d889b2702830f496
Michał <kamykowsky@gmail.com>
2023-10-03 14:35:33 +0200

    translation update

---
+++ lang/pl/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Michał <kamykowsky@gmail.com>
+ */
+$lang['page']                  = 'Strona';
+$lang['date']                  = 'Data';
+$lang['user']                  = 'Użytkownik';
+$lang['desc']                  = 'Opis';
+$lang['diff']                  = 'Różnice';
+$lang['summary']               = 'Edytuj podsumowanie';
+$lang['diff_title']            = 'Pokaż różnice w stosunku do aktualnej wersji';
+$lang['diff_alt']              = 'Pokaż różnice w stosunku do aktualnej wersji';
