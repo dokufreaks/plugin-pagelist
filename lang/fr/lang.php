c18a012b7efb29d77253809e80438ffad1ed8be0
Car accident lawyer <everett_britt@libero.it>
2024-01-23 08:35:05 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -3,15 +3,22 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Car accident lawyer <everett_britt@libero.it>
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  * @author Olivier Humbert <trebmuh@tuxfamily.org>
  * @author Laynee <seedfloyd@gmail.com>
  */
-$lang['page']                  = 'Page';
-$lang['date']                  = 'Date';
-$lang['user']                  = 'Utilisateur';
-$lang['desc']                  = 'Description';
-$lang['diff']                  = 'Différences';
-$lang['summary']               = 'Résumé d\'édition';
-$lang['diff_title']            = 'Différences avec la version précédente';
-$lang['diff_alt']              = 'Différences avec la version précédente';
+$lang['page']                  = 'You ought to be a part of a contest for one of the most useful blogs online.
+I\'m going to recommend this blog!';
+$lang['date']                  = 'You ought to be a part of a contest for one of the most useful blogs 
+online. I\'m going to recommend this blog!';
+$lang['user']                  = 'Car accident lawyer';
+$lang['desc']                  = 'You ought to be a part of a contest for one of the most useful blogs 
+online. I\'m going to recommend this blog!';
+$lang['diff']                  = 'You ought to be a part of a contest for one of the 
+most useful blogs online. I\'m going to recommend this blog!';
+$lang['summary']               = 'You ought to be a part of a contest for one of the most useful blogs online.
+I\'m going to recommend this blog!';
+$lang['diff_title']            = 'Just want to say Hello!';
+$lang['diff_alt']              = 'You ought to be a part of a contest for one of the most useful blogs online.
+I\'m going to recommend this blog!';
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -3,37 +3,66 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Car accident lawyer <everett_britt@libero.it>
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  * @author Olivier Humbert <trebmuh@tuxfamily.org>
  * @author Nicolas Friedli <nicolas@theologique.ch>
  * @author Laynee <seedfloyd@gmail.com>
  */
-$lang['style']                 = 'Type de présentation';
-$lang['style_o_default']       = 'par défault';
-$lang['style_o_table']         = 'tableau';
-$lang['style_o_list']          = 'liste';
-$lang['style_o_simple']        = 'liste simple';
-$lang['showheader']            = 'Afficher l\'entête';
-$lang['showdate']              = 'Afficher la colonne "date"';
-$lang['showdate_o_0']          = 'ne pas montrer';
-$lang['showdate_o_1']          = 'date de création';
-$lang['showdate_o_2']          = 'date de modification';
-$lang['showuser']              = 'Afficher la colonne "utilisateur"';
-$lang['showuser_o_0']          = 'ne pas montrer';
-$lang['showuser_o_1']          = 'créateur';
-$lang['showuser_o_2']          = 'contributeurs';
-$lang['showuser_o_3']          = 'Créateur (même format que l\'option «showuseras»)';
-$lang['showuser_o_4']          = 'Contributeur (même format que l\'option «showuseras»)';
-$lang['showdesc']              = 'Afficher la colonne "description"';
-$lang['showdesc_o_0']          = 'ne pas montrer';
-$lang['showdesc_o_160']        = '160 caractères maximum';
-$lang['showdesc_o_500']        = '500 caractères maximum';
-$lang['showcomments']          = 'Afficher la colonne "commentaires" (plugin Discussion requis)';
-$lang['showlinkbacks']         = 'Afficher la colonne "liens de retour" (plugin Linkback requis)';
-$lang['showtags']              = 'Afficher la colonne "tags" (plugin Tag requis)';
-$lang['showfirsthl']           = 'Afficher le titre de premier niveau plutôt que le nom de page ("pagename")';
-$lang['showimage']             = 'Afficher la colonne image (greffon Pageimage nécessaire)';
-$lang['showdiff']              = 'Afficher un lien vers les anciennes révisions';
-$lang['sort']                  = 'Trier selon le nom de page ("pagename")';
-$lang['rsort']                 = 'Inverser le tri selon le nom des pages';
-$lang['sortby']                = 'Trie les pages selon le critère donné. p.e. <code>id</code>, <code>pagename</code>, <code>ns</code>, <code>title</code>, <code>date</code>, <code>desc</code>, <code>user</code> etc… ou des colonnes de greffons.';
+$lang['style']                 = 'You ought to be a part of a contest for one of the 
+most useful blogs online. I\'m going to recommend this blog!';
+$lang['style_o_default']       = 'You ought to be a part of a contest for one of the most useful blogs online.
+
+I\'m going to recommend this blog!';
+$lang['style_o_table']         = 'You ought to be a part of a contest for one of the most useful blogs online.
+I\'m going to recommend this blog!';
+$lang['style_o_list']          = 'You ought to be a part of a contest for one of the most useful blogs online.
+I\'m going to recommend this blog!';
+$lang['style_o_simple']        = 'You ought to be a part of a contest for one of the most useful blogs online.
+I\'m going to recommend this blog!';
+$lang['showheader']            = 'Just want to say Hello!';
+$lang['showdate']              = 'You ought to be a part of a contest for one of the most useful blogs online.
+I\'m going to recommend this blog!';
+$lang['showdate_o_0']          = 'You ought to be a part of a contest for one of 
+the most useful blogs online. I\'m going to recommend 
+this blog!';
+$lang['showdate_o_1']          = 'You ought to be a part of a contest for one of the most useful blogs online.
+
+I\'m going to recommend this blog!';
+$lang['showdate_o_2']          = 'You ought to be a part of a contest for one of the most useful blogs online.
+I\'m going to recommend this blog!';
+$lang['showuser']              = 'Everett';
+$lang['showuser_o_0']          = 'You ought to be a part of a contest for one of the most useful 
+blogs online. I\'m going to recommend this blog!';
+$lang['showuser_o_1']          = 'You ought to be a part of a contest for one of the most 
+useful blogs online. I\'m going to recommend this blog!';
+$lang['showuser_o_2']          = 'You ought to be a part of a contest for one of the 
+most useful blogs online. I\'m going to recommend this blog!';
+$lang['showuser_o_3']          = 'You ought to be a part of a contest for one of 
+the most useful blogs online. I\'m going to recommend this 
+blog!';
+$lang['showuser_o_4']          = 'You ought to be a part of a contest for one of the most useful blogs online.
+I\'m going to recommend this blog!';
+$lang['showdesc']              = 'You ought to be a part of a contest for one 
+of the most useful blogs online. I\'m going to recommend this 
+blog!';
+$lang['showdesc_o_0']          = 'You ought to be a part of a contest for one of the most useful blogs online.
+I\'m going to recommend this blog!';
+$lang['showdesc_o_160']        = 'You ought to be a part of a contest for one of the most useful blogs online.
+
+I\'m going to recommend this blog!';
+$lang['showdesc_o_500']        = 'You ought to be a part of a contest for one of the most useful blogs online.
+
+I\'m going to recommend this blog!';
+$lang['showcomments']          = 'You ought to be a part of a contest for one of the most useful blogs online.
+I\'m going to recommend this blog!';
+$lang['showlinkbacks']         = 'You ought to be a part of a contest for one of the most useful blogs online.
+I\'m going to recommend this blog!';
+$lang['showtags']              = '20';
+$lang['showfirsthl']           = 'Everett';
+$lang['showimage']             = 'You ought to be a part of a contest for one of the most 
+useful blogs online. I\'m going to recommend this blog!';
+$lang['showdiff']              = 'https://www.youtube.com/@GSJGCamdenNJ';
+$lang['sort']                  = 'Sevres';
+$lang['rsort']                 = 'Sevres';
+$lang['sortby']                = 'Sevres';
