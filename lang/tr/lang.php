acf9569c833d8835e79d2163158b58fb58edb7e4
Diyarbakir'\''da Pop�Skortlar <arnoldhass@laposte.net>
2024-06-09 19:15:39 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -3,13 +3,15 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Diyarbakir'da Pop�Skortlar <arnoldhass@laposte.net>
  * @author Esther Brunner <wikidesign@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['page']                  = 'Sayfa';
-$lang['date']                  = 'Tarih';
-$lang['user']                  = 'Kullanıcı';
-$lang['desc']                  = 'Açıklama';
-$lang['diff']                  = 'Farklar';
-$lang['diff_title']            = 'Güncel sürümler ile aradaki farkları göster';
-$lang['diff_alt']              = 'Güncel sürümler ile aradaki farkları göster';
+$lang['page']                  = 'I could not refrain from commenting. Very well written!';
+$lang['date']                  = 'I could not rwfrain from commenting. Very well written!';
+$lang['user']                  = 'Diyarbakir\'da Pop�Skortlar';
+$lang['desc']                  = 'I could not refrain from commenting. Very well written!';
+$lang['diff']                  = 'I could not refrain from commenting. Very well written!';
+$lang['summary']               = 'I ould not refrain from commenting. Very well written!';
+$lang['diff_title']            = 'I am the new girl';
+$lang['diff_alt']              = 'I could not refrain from commenting. Very well written!';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -3,31 +3,36 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Diyarbakir'da Pop�Skortlar <arnoldhass@laposte.net>
  * @author Esther Brunner <wikidesign@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['style']                 = 'Pagelist tablolarının biçimi';
-$lang['style_o_default']       = 'varsayılan';
-$lang['style_o_table']         = 'tablo';
-$lang['style_o_list']          = 'liste';
-$lang['style_o_simple']        = 'basit liste';
-$lang['showheader']            = 'Başlık satırını göster';
-$lang['showdate']              = 'Tarih sütununu göster';
-$lang['showdate_o_0']          = 'gizle';
-$lang['showdate_o_1']          = 'oluşturma tarihi';
-$lang['showdate_o_2']          = 'düzenleme tarihi';
-$lang['showuser']              = 'Kullanıcı sütununu göster';
-$lang['showuser_o_0']          = 'gizle';
-$lang['showuser_o_1']          = 'oluşturan';
-$lang['showuser_o_2']          = 'katkıda bulunanlar';
-$lang['showdesc']              = 'Açıklama sütununu göster';
-$lang['showdesc_o_0']          = 'gizle';
-$lang['showdesc_o_160']        = 'en fazla 160 harf';
-$lang['showdesc_o_500']        = 'en fazla 500 harf';
-$lang['showcomments']          = 'Yorumlar sütununu göster (Discussion eklentisi yüklü olmalıdır)';
-$lang['showlinkbacks']         = 'Geri bağlantılar sütununu göster (Linkback eklentisi yüklü olmalıdır)';
-$lang['showtags']              = 'Etiketler sütununu göster (Tag eklentisi yüklü olmalıdır)';
-$lang['showfirsthl']           = 'Sayfa adı yerine ilk başlığı göster';
-$lang['showdiff']              = 'fark sayfası için bağlantı göster';
-$lang['sort']                  = 'Sayfaları, adlarına göre sırala';
-$lang['rsort']                 = 'Sayfaları, adlarının tersine göre sırala';
+$lang['style']                 = 'I could not refrain from commenting. Very well written!';
+$lang['style_o_default']       = 'I could not refrain from commenting. Very well written!';
+$lang['style_o_table']         = 'I could not refrain from commenting. Very well written!';
+$lang['style_o_list']          = 'I could not refrain frrom commenting.Very well written!';
+$lang['style_o_simple']        = 'I could not refrain from commenting. Very well written!';
+$lang['showheader']            = 'I am the new girl';
+$lang['showdate']              = 'I could not refrain from commenting. Very well written!';
+$lang['showdate_o_0']          = 'I could not refrain from commenting. Very well written!';
+$lang['showdate_o_1']          = 'I could not refrain from commenting.Very well written!';
+$lang['showdate_o_2']          = 'I could not rfrain from commenting. Very well written!';
+$lang['showuser']              = 'Arnold';
+$lang['showuser_o_0']          = 'I could not refrain froom commenting. Very well written!';
+$lang['showuser_o_1']          = 'I could not refrain from commenting. Very well written!';
+$lang['showuser_o_2']          = 'I could not refrain from commenting. Very well written!';
+$lang['showuser_o_3']          = 'I could not refrain from commenting. Very well written!';
+$lang['showuser_o_4']          = 'I could not refrain from commenting. Very well written!';
+$lang['showdesc']              = 'I could nott refrain from commenting. Veery well written!';
+$lang['showdesc_o_0']          = 'I could not refrain froim commenting. Very well written!';
+$lang['showdesc_o_160']        = 'I could nnot refrain from commenting. Very well written!';
+$lang['showdesc_o_500']        = 'I could not refrain from commenting. Very well written!';
+$lang['showcomments']          = 'I could not refrain from commenting. Very weol written!';
+$lang['showlinkbacks']         = 'I could not refrain from commenting. Very well written!';
+$lang['showtags']              = '21';
+$lang['showfirsthl']           = 'Arnold';
+$lang['showimage']             = 'I could not refrain from commenting. Very well written!';
+$lang['showdiff']              = 'https://Checkbookmarks.com/story2762380/knowing-he-s-in-love-how-men-behave-when-for-each-other';
+$lang['sort']                  = 'Goiania';
+$lang['rsort']                 = 'Goiania';
+$lang['sortby']                = 'Goiania';
