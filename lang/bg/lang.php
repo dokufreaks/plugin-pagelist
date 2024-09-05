1f8a8920d4f5f27309f4a07fb44ba48508879e62
bclub.tk <allen_rasco@gmail.com>
2024-09-05 20:20:05 +0200

    translation update

--- lang/bg/lang.php
+++ lang/bg/lang.php
@@ -3,12 +3,44 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author bclub.tk <allen_rasco@gmail.com>
  * @author Kiril <neohidra@gmail.com>
  */
-$lang['page']                  = 'Страница';
-$lang['date']                  = 'Дата';
-$lang['user']                  = 'Потребител';
-$lang['desc']                  = 'Описание';
-$lang['diff']                  = 'Разлики';
-$lang['diff_title']            = 'Покажи разликите с текущата версия';
-$lang['diff_alt']              = 'Покажи разликите с текущата версия';
+$lang['page']                  = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to 
+and you\'re just extremely wonderful. I really like what you have acquired here, really like what you\'re stating and the way in which you say 
+it. You make it enjoyable and you still care for to keep it smart.
+I can not wait to read much more from you.
+This is really a wonderful website.';
+$lang['date']                  = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just extremely wonderful.
+I really like what you have acquired here, really like what you\'re stating and the way 
+in which you say it. You make it enjoyable and you still care for to keep it smart.
+I can not wait to read much more from you. This is 
+really a wonderful website.';
+$lang['user']                  = 'bclub.tk';
+$lang['desc']                  = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to 
+and you\'re just extremely wonderful. I really like what you have acquired here, really like what you\'re stating and the way 
+in which you say it. You make it enjoyable and you still 
+care for to keep it smart. I can not wait to read much more from 
+you. This is really a wonderful website.';
+$lang['diff']                  = 'https://bclub.ski
+Wonderful goods from you, man. I have understand 
+your stuff previous to and you\'re just extremely wonderful.
+I really like what you have acquired here, really like what you\'re stating and the way in which you say it.
+You make it enjoyable and you still care for to keep it smart.
+I can not wait to read much more from you. This is really a 
+wonderful website.';
+$lang['summary']               = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just 
+extremely wonderful. I really like what you have acquired 
+here, really like what you\'re stating and the way in which you 
+say it. You make it enjoyable and you still care for to keep it smart.
+I can not wait to read much more from you. This is really a wonderful website.';
+$lang['diff_title']            = 'https://bclub.skiI am the new girl https://bclub.ski';
+$lang['diff_alt']              = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just extremely wonderful.
+I really like what you have acquired here, really like what you\'re 
+stating and the way in which you say it. You make it enjoyable and you still care for to keep it smart.
+I can not wait to read much more from you. This is really a wonderful website.';
--- lang/bg/settings.php
+++ lang/bg/settings.php
@@ -3,30 +3,155 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author bclub.tk <allen_rasco@gmail.com>
  * @author Kiril <neohidra@gmail.com>
  */
-$lang['style']                 = 'Стил';
-$lang['style_o_default']       = 'стандартен';
-$lang['style_o_table']         = 'табличен';
-$lang['style_o_list']          = 'таблица/списък';
-$lang['style_o_simple']        = 'списък';
-$lang['showheader']            = 'Показване на заглавния ред';
-$lang['showdate']              = 'Показване на колона за дата';
-$lang['showdate_o_0']          = 'скрий';
-$lang['showdate_o_1']          = 'дата на създаване';
-$lang['showdate_o_2']          = 'дата на редактиране';
-$lang['showuser']              = 'Показване на колона за потребител';
-$lang['showuser_o_0']          = 'скрий';
-$lang['showuser_o_1']          = 'създател';
-$lang['showuser_o_2']          = 'допринесли';
-$lang['showdesc']              = 'Показване на колона за описание';
-$lang['showdesc_o_0']          = 'скрий';
-$lang['showdesc_o_160']        = 'максимум 160 символа';
-$lang['showdesc_o_500']        = 'максимум 500 символа';
-$lang['showcomments']          = 'Показвай колона за коментари (изисква приставката Discussion)';
-$lang['showlinkbacks']         = 'Показвай колона за linkbacks (изисква приставката Linkback )';
-$lang['showtags']              = 'Показвай колона за етикети (изисква приставката Tag)';
-$lang['showfirsthl']           = 'Показвай първото заглавие вместо името на страницата';
-$lang['showdiff']              = 'Показвай линк към страница с разликите';
-$lang['sort']                  = 'Сортиране на страниците по име на страница';
-$lang['rsort']                 = 'Обратен ред на сортиране по име на страница';
+$lang['style']                 = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just 
+extremely wonderful. I really like what you have acquired here, really like what you\'re stating and 
+the way in which you say it. You make it enjoyable and you still 
+care for to keep it smart. I can not wait to read much more from you.
+
+This is really a wonderful website.';
+$lang['style_o_default']       = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just extremely wonderful.
+I really like what you have acquired here, really like what 
+you\'re stating and the way in which you say it.
+
+You make it enjoyable and you still care for to keep it smart.
+I can not wait to read much more from you. This is really a wonderful website.';
+$lang['style_o_table']         = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just 
+extremely wonderful. I really like what you have acquired 
+here, really like what you\'re stating and the way in which you say it.
+You make it enjoyable and you still care for to 
+keep it smart. I can not wait to read much more from you.
+This is really a wonderful website.';
+$lang['style_o_list']          = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your 
+stuff previous to and you\'re just extremely wonderful.
+I really like what you have acquired here, really like what you\'re stating and the way in which you say it.
+You make it enjoyable and you still care for to keep 
+it smart. I can not wait to read much more from you.
+This is really a wonderful website.';
+$lang['style_o_simple']        = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just extremely wonderful.
+I really like what you have acquired here,
+really like what you\'re stating and the way in which you say it.
+You make it enjoyable and you still care for to keep it smart.
+I can not wait to read much more from you. This is really a wonderful 
+website.';
+$lang['showheader']            = 'https://bclub.skiI am the new girl https://bclub.ski';
+$lang['showdate']              = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just extremely wonderful.
+I really like what you have acquired here, really like what you\'re 
+stating and the way in which you say it. You make it enjoyable and you still care for to keep it smart.
+I can not wait to read much more from you. This is really a wonderful website.';
+$lang['showdate_o_0']          = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and 
+you\'re just extremely wonderful. I really like what you have acquired here, really like what you\'re stating and the way in which you say it.
+You make it enjoyable and you still care for 
+to keep it smart. I can not wait to read much more from you.
+
+This is really a wonderful website.';
+$lang['showdate_o_1']          = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just extremely wonderful.
+I really like what you have acquired here, really 
+like what you\'re stating and the way in which 
+you say it. You make it enjoyable and you still care for to keep it smart.
+I can not wait to read much more from you.
+
+This is really a wonderful website.';
+$lang['showdate_o_2']          = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re 
+just extremely wonderful. I really like what you have 
+acquired here, really like what you\'re stating and the way in which you say it.
+
+You make it enjoyable and you still care for to keep it smart.
+I can not wait to read much more from you.
+This is really a wonderful website.';
+$lang['showuser']              = 'Allen';
+$lang['showuser_o_0']          = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just extremely wonderful.
+I really like what you have acquired here, really like what you\'re stating and 
+the way in which you say it. You make it enjoyable and you still care for to keep it smart.
+I can not wait to read much more from you. This is really a wonderful website.';
+$lang['showuser_o_1']          = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just extremely wonderful.
+I really like what you have acquired here, really like what you\'re stating and the 
+way in which you say it. You make it enjoyable and you still care for to keep 
+it smart. I can not wait to read much more from you.
+This is really a wonderful website.';
+$lang['showuser_o_2']          = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and 
+you\'re just extremely wonderful. I really like what you have acquired here, really like what you\'re stating and the way in which 
+you say it. You make it enjoyable and you still care 
+for to keep it smart. I can not wait to read much more 
+from you. This is really a wonderful website.';
+$lang['showuser_o_3']          = 'https://bclub.ski
+Wonderful goods from you, man. I have understand 
+your stuff previous to and you\'re just extremely wonderful.
+I really like what you have acquired here, really like what you\'re stating and 
+the way in which you say it. You make it enjoyable and you still care 
+for to keep it smart. I can not wait to read much more from 
+you. This is really a wonderful website.';
+$lang['showuser_o_4']          = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous 
+to and you\'re just extremely wonderful. I really like what you have acquired here, really like what you\'re stating and the way in which you say it.
+You make it enjoyable and you still care for to keep it smart.
+
+I can not wait to read much more from you. This is really a wonderful website.';
+$lang['showdesc']              = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to 
+and you\'re just extremely wonderful. I really like what you have acquired here,
+really like what you\'re stating and the way in which you say it.
+
+You make it enjoyable and you still care for to keep it smart.
+I can not wait to read much more from you. This is really a wonderful website.';
+$lang['showdesc_o_0']          = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just extremely wonderful.
+
+I really like what you have acquired here, really like what you\'re stating 
+and the way in which you say it. You make it enjoyable and you still care 
+for to keep it smart. I can not wait to read much more from you.
+This is really a wonderful website.';
+$lang['showdesc_o_160']        = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to 
+and you\'re just extremely wonderful. I really like what you have acquired here, really like 
+what you\'re stating and the way in which you say it.
+You make it enjoyable and you still care for to keep it smart.
+I can not wait to read much more from you. This is really a 
+wonderful website.';
+$lang['showdesc_o_500']        = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just extremely wonderful.
+I really like what you have acquired here, really like what you\'re stating and the way in which you say it.
+You make it enjoyable and you still care for to keep it smart.
+I can not wait to read much more from you. This is really a wonderful 
+website.';
+$lang['showcomments']          = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just extremely wonderful.
+I really like what you have acquired here, really like what you\'re 
+stating and the way in which you say it. You make it enjoyable and you 
+still care for to keep it smart. I can not 
+wait to read much more from you. This is really a 
+wonderful website.';
+$lang['showlinkbacks']         = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just extremely 
+wonderful. I really like what you have acquired here, really like what you\'re stating and the way in which 
+you say it. You make it enjoyable and you still care for to 
+keep it smart. I can not wait to read much more from 
+you. This is really a wonderful website.';
+$lang['showtags']              = '20';
+$lang['showfirsthl']           = 'Allen';
+$lang['showimage']             = 'https://bclub.ski
+Wonderful goods from you, man. I have understand your stuff previous to and you\'re just extremely 
+wonderful. I really like what you have acquired here, 
+really like what you\'re stating and the way in which you say it.
+
+You make it enjoyable and you still care for to keep it smart.
+I can not wait to read much more from you.
+This is really a wonderful website.';
+$lang['showdiff']              = 'https://bclub.ski';
+$lang['sort']                  = 'Charlton';
+$lang['rsort']                 = 'Charlton';
+$lang['sortby']                = 'Charlton';
