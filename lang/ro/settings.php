e3ffefd66e5d75dd0331a79ab07b0d997b912af5
Tiberiu Popescu <popescu.tibi@gmail.com>
2025-04-03 17:25:45 +0200

    translation update

--- lang/ro/settings.php
+++ lang/ro/settings.php
@@ -3,11 +3,35 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Tiberiu Popescu <popescu.tibi@gmail.com>
  * @author Mircea Simion <mircea.simion@gmail.com>
  */
+$lang['style']                 = 'Stilul listei de pagini';
 $lang['style_o_default']       = 'implicit';
 $lang['style_o_table']         = 'tabel';
-$lang['style_o_list']          = 'tabel/lista';
-$lang['style_o_simple']        = 'lista simpla';
+$lang['style_o_list']          = 'tabel/listă';
+$lang['style_o_simple']        = 'listă simplă';
+$lang['showheader']            = 'arată linia de titlu';
+$lang['showdate']              = 'arată coloana dată';
 $lang['showdate_o_0']          = 'ascunde';
+$lang['showdate_o_1']          = 'data creării';
+$lang['showdate_o_2']          = 'data modificării';
+$lang['showuser']              = 'arată coloana utilizator';
 $lang['showuser_o_0']          = 'ascunde';
+$lang['showuser_o_1']          = 'creator';
+$lang['showuser_o_2']          = 'contribuitori';
+$lang['showuser_o_3']          = 'creator (în format "arată utilizatorul ca")';
+$lang['showuser_o_4']          = 'contribuitori (în format "arată utilizatorul ca")';
+$lang['showdesc']              = 'arată coloana descriere';
+$lang['showdesc_o_0']          = 'ascunde';
+$lang['showdesc_o_160']        = 'max. 160 de caractere';
+$lang['showdesc_o_500']        = 'max. 500 de caractere';
+$lang['showcomments']          = 'arată coloana comentarii (necesită plugin-ul Discussion)';
+$lang['showlinkbacks']         = 'arată coloana linkback (necesită plugin-ul Linkback)';
+$lang['showtags']              = 'arată coloana etichete (necesită plugin-ul Tag)';
+$lang['showfirsthl']           = 'arată titlul în loc de numele paginii';
+$lang['showimage']             = 'arată coloana imagine (necesită plugin-ul Pageimage)';
+$lang['showdiff']              = 'arată link la pagina cu diferențe';
+$lang['sort']                  = 'sortează paginile după nume';
+$lang['rsort']                 = 'sortează invers paginile după nume';
+$lang['sortby']                = 'sortează pagina după elementul specificat ex. <code>id</code>, <code>pagename</code>, <code>ns</code>, <code>title</code>, <code>date</code>, <code>desc</code>, <code>user</code> etc sau după coloane din plugin-uri';
